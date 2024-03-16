36fbebc228dc8f64f4a954f9a0ab5224a8ee10ca
Sadanand Jaiswal <sadanandjaiswal92@gmail.com>
2024-03-16 20:48:38 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,73 @@
+import express from "express"
+import bodyParser from "body-parser"
+import mongoose from "mongoose"
+import cors from "cors"
+import multer from "multer"
+import helmet from "helmet"
+import dotenv from "dotenv"
+import morgan from "morgan"
+import path from "path"
+import {fileURLToPath} from "url"
+import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+import {register} from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+// import User from "./models/User.js";
+// import Post from "./models/Post.js";
+// import { users, posts } from "./data/index.js";
+
+// we have imported path and url and __filename , __dirname as the type is set to module in cjs (comman js) we don't need to import this 
+
+// CONFIGURATIONS
+const __filename  = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+// File Storage
+const storage = multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null, "public/assets");
+    },
+    filename: function(req, file, cb){
+        cb(null, file.originalname);
+    }
+})
+const upload = multer({storage}); // any time we need to upload we will use this
+
+// Routes With Files
+app.post('/auth/register', upload.single("picture"), register);
+app.post('/posts', verifyToken, upload.single("picture"), createPost);
+
+// Routes
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
+
+//  DATABASE SETUP
+const PORT = process.env.PORT || 5000;
+mongoose.connect(process.env.MONGO_URL,{
+    // useNewUrlParse: true,
+    // useUnifiedTopology: true
+}).then(()=>{
+    app.listen(PORT,()=>{
+        console.log('Database Connected');
+        console.log(`Server Running on Port ${PORT}`);
+    })
+
+    // User.insertMany(users);
+    // Post.insertMany(posts);
+}).catch((e)=>{
+    console.error(e, " did not connected to server")
+})
+
